e251bca5db781682addda69a7cec1927c9b10ad1
cyh756085049 <756085049@qq.com>
2024-01-17 22:44:35 +0800

    feat: 打乱数组

---
+++ algorithms/leetcode/sort/shuffle.js
@@ -0,0 +1,29 @@
+// 打乱一个没有重复元素的数组
+const Solution = function (nums) {
+    this.nums = nums;
+}
+
+Solution.prototype.reset = () => {
+    return this.nums;
+}
+
+Solution.prototype.shuffle = function () {
+    const res = [...this.nums];
+    for (let i = res.length - 1; i >= 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        swap(res, randomIndex, i);
+    }
+    return res;
+}
+
+const swap = (arr, i, j) => {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+const nums = [1, 2, 3];
+const solution = new Solution(nums);
+console.log(solution.shuffle());
+solution.reset();
+console.log(solution.shuffle());
