e37b4b8b89853c679e4885e63a97c79a5f4c5c56
cyh756085049 <756085049@qq.com>
2024-05-05 20:38:59 +0800

    和为k的子数组

--- algorithms/leetcode/substring/q560_subArraySum.js
+++ algorithms/leetcode/substring/q560_subArraySum.js
@@ -1,6 +1,7 @@
 /**
  * 560. 和为 K 的子数组 https://leetcode.cn/problems/subarray-sum-equals-k/description/?envType=study-plan-v2&envId=top-100-liked
  * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的连续子数组的个数 。
+ * 题解：https://leetcode.cn/problems/subarray-sum-equals-k/?envType=study-plan-v2&envId=top-100-liked
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
