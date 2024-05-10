a9c54c36da094d28feb15ef9e90e0b339fa50adf
cyh756085049 <756085049@qq.com>
2024-05-10 21:41:20 +0800

    k个一组翻转链表

--- algorithms/leetcode/list-node/q25_reverseKGroup.js
+++ algorithms/leetcode/list-node/q25_reverseKGroup.js
@@ -3,6 +3,14 @@ function ListNode(val, next) {
     this.next = next === undefined ? null : next;
 }
 
+/**
+ * 25. K 个一组翻转链表 https://leetcode.cn/problems/reverse-nodes-in-k-group/description/?envType=study-plan-v2&envId=top-100-liked
+ * 思路：先找到k个一组的尾节点，然后将这组节点翻转，连接
+ * 题解：https://leetcode.cn/problems/reverse-nodes-in-k-group/solutions/10416/tu-jie-kge-yi-zu-fan-zhuan-lian-biao-by-user7208t/?envType=study-plan-v2&envId=top-100-liked
+ * @param head
+ * @param k
+ * @return {*}
+ */
 const reverseKGroup = (head, k) => {
     const dummpy = new ListNode(0, head);
     let pre = end = dummpy;
@@ -18,11 +26,16 @@ const reverseKGroup = (head, k) => {
             break;
         }
 
+        // 先记录下end.next,方便后面链接链表
         const next = end.next;
+        // 然后断开链表
         end.next = null;
 
+        // 记录下要翻转链表的头节点
         const start = pre.next;
+        // 翻转链表
         pre.next = reverseListNode(start);
+        // 翻转后头节点变到最后。通过.next把断开的链表重新链接
         start.next = next;
 
         pre = start;
