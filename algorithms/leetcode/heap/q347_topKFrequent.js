173a83d0546cf2e071d72440fd535f79dd33762e
cyh756085049 <756085049@qq.com>
2024-06-20 09:11:04 +0800

    寻找两个正序数组的中位数

--- algorithms/leetcode/heap/q347_topKFrequent.js
+++ algorithms/leetcode/heap/q347_topKFrequent.js
@@ -6,7 +6,7 @@
  * @param k
  * @return {any[]}
  */
-const topKFrequent = (nums, k) => {
+const q347_topKFrequent = (nums, k) => {
     const map = new Map();
     for (let i = 0; i < nums.length; i++) {
         map.set(nums[i], (map.get(nums[i]) || 0) + 1);
@@ -16,4 +16,4 @@ const topKFrequent = (nums, k) => {
     return newArray.sort((a, b) => map.get(b) - map.get(a)).slice(0, k);
 }
 
-console.log(topKFrequent([1,1,1,2,2,3], 2));
+console.log(q347_topKFrequent([1,1,1,2,2,3], 2));
